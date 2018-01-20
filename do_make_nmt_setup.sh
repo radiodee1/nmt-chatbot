3962e95a2104e4086ff87b53bf04909799490ca2
David Liebman <david.c.liebman@gmail.com>
2018-01-20 11:48:13 -0500

    special setup script

---
+++ do_make_nmt_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "you want to put your data files in the new_data/ folder"
+echo "then run this command again."
+echo "this sets up a pair of vocabulary files. You don't want to"
+echo "change them after you start training... even if you change"
+echo "your train.from and train.to files."
+
+cd setup
+python3 prepare_data.py
+cd ..
+
