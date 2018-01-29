d0be8768c9cbf81629abdc7f7ef47e32c7422d5a
David Liebman <david.c.liebman@gmail.com>
2018-01-29 07:56:15 -0500

    some scripts

---
+++ do_launch_nmt_glove_chat.sh
@@ -0,0 +1,27 @@
+
+
+cd nmt
+python3 -m nmt.nmt \
+    --attention=scaled_luong \
+    --src=from --tgt=to \
+    --vocab_prefix=../data/vocab  \
+    --train_prefix=../data/train \
+    --dev_prefix=../data/test  \
+    --test_prefix=../data/test \
+    --out_dir=../model \
+    --num_train_steps=12000 \
+    --steps_per_stats=10 \
+    --infer_batch_size=10 \
+    --num_layers=2 \
+    --num_units=300 \
+    --dropout=0.2 \
+    --metrics=bleu \
+    --encoder_type=bi \
+    --beam_width=10 \
+    --length_penalty_weight=1.0 \
+    --batch_size=16 \
+    --optimizer=adam \
+#    --override_loaded_hparams=True
+#    --num_gpus=0 \
+
+
