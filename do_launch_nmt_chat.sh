4b5db25be8db913e4320339344f517f74ced4e71
David Liebman <david.c.liebman@gmail.com>
2018-02-02 08:04:07 -0500

    launch

--- do_launch_nmt_chat.sh
+++ do_launch_nmt_chat.sh
@@ -10,17 +10,18 @@ python3 -m nmt.nmt \
     --test_prefix=../data/test \
     --out_dir=../model \
     --num_train_steps=12000 \
-    --steps_per_stats=10 \
-    --infer_batch_size=10 \
+    --steps_per_stats=100 \
+    --infer_batch_size=32 \
     --num_layers=2 \
-    --num_units=512 \
+    --num_units=300 \
     --dropout=0.2 \
     --metrics=bleu \
     --encoder_type=bi \
     --beam_width=10 \
     --length_penalty_weight=1.0 \
-    --batch_size=16 \
+    --batch_size=128 \
     --optimizer=adam \
+    --learning_rate=0.001
 #    --override_loaded_hparams=True
 #    --num_gpus=0 \
  
